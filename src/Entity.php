6e3dad731a75d1cc89e0d817ee5f433c5d1ab668
mimosafa <mimosafa@gmail.com>
2017-12-12 03:59:36 +0900

    update for attributes class as template
    not refer but clone

--- src/Entity.php
+++ src/Entity.php
@@ -66,10 +66,13 @@ abstract class Entity {
             return;
         }
         $attrClass = str_replace( __NAMESPACE__, __NAMESPACE__ . '\\Attributes', get_called_class() );
-        if ( ! is_object( $args ) || ! is_a( $args, $attrClass ) ) {
-            $args = new $attrClass( wp_parse_args( $args ) );
+        if ( is_a( $args,$attrClass ) ) {
+            $attrs = clone $args;
         }
-        return static::$instances[$name] = new static( $name, $args );
+        else {
+            $attrs = new $attrClass( wp_parse_args( $args ) );
+        }
+        return static::$instances[$name] = new static( $name, $attrs );
     }
 
     /**
